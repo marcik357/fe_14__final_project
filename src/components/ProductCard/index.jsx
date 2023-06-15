a9c3bd0043cf4cb80b6af85122c374f4178ff4b2
Yan-Yaremenko <luxray0@gmail.com>
2023-06-15 21:07:40 +0300

    ADD: component "productCard"

---
+++ src/components/ProductCard/index.jsx
@@ -0,0 +1,41 @@
+import styles from "./productCard.module.scss";
+
+function ProductCard(props) {
+  const { url, userIcon, creator, verifiedIcon, currentBid, price } = props;
+
+  return (
+    <>
+      <div className={styles.productCard}>
+        <img className={styles.productCard__img} src={url} alt="" />
+        <div className={styles.productCard__userInfo}>
+          <div className={styles.productCard__userInfo_items}>
+            <img
+              className={styles.productCard__userInfo_userIcon}
+              src={userIcon}
+              alt="user-avatar"
+            />
+            <p className={styles.productCard__userInfo_creator}>@{creator}</p>
+          </div>
+
+          <img
+            className={styles.productCard__userInfo_verifiedIcon}
+            src={verifiedIcon}
+            alt="verified-icon"
+          />
+        </div>
+        <div className={styles.productCard__priceInfo}>
+          <div className={styles.productCard__priceInfo_currentBid}>
+            <p>Current bid</p>
+            <span>{currentBid} ETH</span>
+          </div>
+          <div className={styles.productCard__priceInfo_buyNow}>
+            <p>Buy now</p>
+            <span>{price} ETH</span>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default ProductCard;
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,29 @@
-// import style from './home.module.scss'
+import styles from "./home.module.scss";
+import { useState, useEffect } from "react";
+import ProductCard from "../../components/ProductCard";
 
 export function Home() {
+  const [products, setProducts] = useState([]);
 
-    return (
-        <>
-            {/* <div className={style.home}>
-            </div> */}
-        </>
-    )
+  useEffect(() => {
+    fetch("./data/productList.json")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setProducts(data.products);
+      });
+  }, []);
+  return (
+    <>
+      <div className={styles.home}>
+        <div className={styles.home__title}>
+          <h2>NFTs</h2>
+        </div>
+        {products.map((product) => (
+          <ProductCard {...product} key={product.id} />
+        ))}
+      </div>
+    </>
+  );
 }
