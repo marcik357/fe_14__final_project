09e638bf1f9f5fc97a85c27bfd11a281d8350fa8
Олександр Поплавський <o.poplavskiy@agro.corp>
2023-06-18 23:39:19 +0300

    Add: component Blog

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,8 +1,31 @@
-import styles from './blog.module.scss'
+import styles from "./blog.module.scss"
+import Data from '../../data.json';
+// import {Link} from "react-router-dom";
+import MainImg from '../../img/img-blog-main.png'
+
 export default function Blog() {
+
   return (
-    <div className={styles.testComponent}>
-      <div className={styles.testcomponentCube}>Blog</div>
+    <div>
+      <img src={MainImg}></img>
+
+      {
+        Data.map( data => {
+          return(
+            <div>
+              <div>
+                <img src={data.url} alt=""/>
+                <h3>{data.capturePost}</h3>
+                <img src={data.iconPost} alt=""/>
+                <p>{data.authorPost}</p>
+                <p>{data.dataPost}</p>
+                <p>{data.textPost}</p>
+              </div>
+            </div>
+          )
+        })
+      }
+
     </div>
-  );
+  )
 }
