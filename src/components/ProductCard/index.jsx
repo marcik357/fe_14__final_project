d89532c0289c4f091066a7eb5e6c457ff63f664f
Yan-Yaremenko <luxray0@gmail.com>
2023-06-20 18:28:53 +0300

    ADD: pagination, Loader component

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -1,43 +1,54 @@
 import styles from './productCard.module.scss';
-
+import Loader from '../Loader';
 function ProductCard(props) {
-  const {
-    url, userIcon, creator, verifiedIcon, price,
-  } = props;
+  const { url, userIcon, creator, verifiedIcon, price } = props;
 
   return (
     <div className={styles.productCard}>
-      <img className={styles.productCard__img} src={url} alt="" />
+      {url ? (
+        <Loader />
+      ) : (
+        <Loader />
+      )}
       <div className={styles.productCard__userInfo}>
         <div className={styles.productCard__userInfo_items}>
           <img
             className={styles.productCard__userInfo_userIcon}
             src={userIcon}
-            alt="user-avatar"
+            alt='user-avatar'
           />
-          <p className={styles.productCard__userInfo_creator}>
-            @
-            {creator}
-          </p>
+          <p className={styles.productCard__userInfo_creator}>@{creator}</p>
         </div>
 
         <img
           className={styles.productCard__userInfo_verifiedIcon}
           src={verifiedIcon}
-          alt="verified-icon"
+          alt='verified-icon'
         />
       </div>
       <div className={styles.productCard__priceInfo}>
         <div className={styles.productCard__priceInfo_price}>
-          
-          <button className={styles.productCard__priceInfo_button} type="submit">Buy now</button>
+          <button
+            className={styles.productCard__priceInfo_button}
+            type='submit'
+          >
+            Buy now
+          </button>
         </div>
         <div className={styles.productCard__priceInfo_buyNow}>
-          <svg fill="#000000" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg">
-            <g id="SVGRepo_bgCarrier" strokeWidth="0" />
-            <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" />
-            <g id="SVGRepo_iconCarrier">
-              <path d="M15.927 23.959l-9.823-5.797 9.817 13.839 9.828-13.839-9.828 5.797zM16.073 0l-9.819 16.297 9.819 5.807 9.823-5.801z" />
+          <svg
+            fill='#000000'
+            viewBox='0 0 32 32'
+            xmlns='http://www.w3.org/2000/svg'
+          >
+            <g id='SVGRepo_bgCarrier' strokeWidth='0' />
+            <g
+              id='SVGRepo_tracerCarrier'
+              strokeLinecap='round'
+              strokeLinejoin='round'
+            />
+            <g id='SVGRepo_iconCarrier'>
+              <path d='M15.927 23.959l-9.823-5.797 9.817 13.839 9.828-13.839-9.828 5.797zM16.073 0l-9.819 16.297 9.819 5.807 9.823-5.801z' />
             </g>
           </svg>
           <p>
--- src/components/ProductList/index.jsx
+++ src/components/ProductList/index.jsx
@@ -2,11 +2,26 @@ import { connect } from 'react-redux';
 import { useEffect } from 'react';
 import ProductCard from '../ProductCard';
 import styles from './productList.module.scss';
+import './pagination.scss';
 import { getDataAsync } from '../../redux/actions/getDataAction';
-
+import usePagination from '../../Hooks/usePagination';
+import Loader from '../Loader';
 function ProductList(props) {
   const { products, getDataAsync } = props;
-  
+
+  const {
+    firstContentIndex,
+    lastContentIndex,
+    nextPage,
+    prevPage,
+    page,
+    setPage,
+    totalPages,
+  } = usePagination({
+    contentPerPage: 12,
+    count: products?.length,
+  });
+
   useEffect(() => {
     getDataAsync();
   }, [getDataAsync]);
@@ -17,9 +32,26 @@ function ProductList(props) {
         <h2>NFTs</h2>
       </div>
       <div className={styles.products__wrapper}>
-        {products.map((product) => (
+        {products?.slice(firstContentIndex, lastContentIndex).map((product) => (
           <ProductCard {...product} key={product.id} />
         ))}
+      </div>
+      <div className='pagination'>
+        <button onClick={prevPage} className='page'>
+          &#171;
+        </button>
+        {[...Array(totalPages).keys()].map((el) => (
+          <button
+            onClick={() => setPage(el + 1)}
+            key={el}
+            className={`page ${page === el + 1 ? 'active' : ''}`}
+          >
+            {el + 1}
+          </button>
+        ))}
+        <button onClick={nextPage} className='page'>
+          &#187;
+        </button>
       </div>
     </div>
   ) : (
