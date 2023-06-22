185e5915725f8f3a8dadcbf22dad1297b8af9753
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-06-22 15:57:22 +0300

    UPDATE: add content and sidebar code in index.jsx, add styles

--- src/components/Filter/index.jsx
+++ src/components/Filter/index.jsx
@@ -11,7 +11,8 @@ function Filter() {
   const openModal = () => {
     setIsOpen(true);
   };
-  const closeModal = () => {
+  const closeModal = (e) => {
+    e.preventDefault();
     setIsOpen(false);
   };
   return (
@@ -19,7 +20,7 @@ function Filter() {
       <div className={styles.filter__container}>
         <h2 className={styles.filter__title}>NFTs</h2>
         <div className={styles.filter__wrapper}>
-          <button className={styles.filter__button} type="button" onClick={openModal}>Filters</button>
+          <button className={styles.filter__openBtn} type="button" onClick={openModal}>Filters</button>
           <div className={`${styles.filter__sideWrapperBckg} ${isOpen ? styles.open : ''}`} onClick={closeModal}>
             <div className={`${styles.filter__sideWrapper} ${isOpen ? styles.open : ''}`}>
               <div className={styles.filter__sideHeader}>
@@ -36,29 +37,17 @@ function Filter() {
                     All
                   </label>
                 </div>
-                <h4 className={styles.filter__sideCategoryTitle}>Collection</h4>
+                <h4 className={styles.filter__sideCategoryTitle}>Price</h4>
                 <div className={styles.filter__sideItem}>
-                  <label htmlFor="animals">
-                    <input type="checkbox" id="animals" name="animals" />
-                    Animals
+                  <label htmlFor="min">
+                    <input type="checkbox" id="min" name="min" />
+                    Min
                   </label>
                 </div>
                 <div className={styles.filter__sideItem}>
-                  <label htmlFor="future">
-                    <input type="checkbox" id="future" name="future" />
-                    Future
-                  </label>
-                </div>
-                <div className={styles.filter__sideItem}>
-                  <label htmlFor="nature">
-                    <input type="checkbox" id="nature" name="nature" />
-                    Nature
-                  </label>
-                </div>
-                <div className={styles.filter__sideItem}>
-                  <label htmlFor="space">
-                    <input type="checkbox" id="space" name="space" />
-                    Space
+                  <label htmlFor="max">
+                    <input type="checkbox" id="max" name="max" />
+                    Max
                   </label>
                 </div>
                 <h4 className={styles.filter__sideCategoryTitle}>Author</h4>
@@ -86,6 +75,50 @@ function Filter() {
                     Travis
                   </label>
                 </div>
+                <h4 className={styles.filter__sideCategoryTitle}>Collection</h4>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="animals">
+                    <input type="checkbox" id="animals" name="animals" />
+                    Animals
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="future">
+                    <input type="checkbox" id="future" name="future" />
+                    Future
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="nature">
+                    <input type="checkbox" id="nature" name="nature" />
+                    Nature
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="space">
+                    <input type="checkbox" id="space" name="space" />
+                    Space
+                  </label>
+                </div>
+              </div>
+              <div className={styles.filter__sideFooter}>
+                <button className={styles.filter__applyBtn} type="button">Apply</button>
+              </div>
+            </div>
+          </div>
+          <div className={styles.filter__content}>
+            <section className={styles.filter__contentList}>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quis, ea ipsam ab mollitia quo sunt voluptatibus quam dignissimos eaque, optio molestias atque amet harum impedit quasi commodi error cupiditate aliquid?</p>
+            </section>
+            <aside className={styles.filter__contentSidebar}>
+              <div className={styles.filter__sideBody}>
+                <h4 className={styles.filter__sideCategoryTitle}>All</h4>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="all">
+                    <input type="checkbox" id="all" name="all" />
+                    All
+                  </label>
+                </div>
                 <h4 className={styles.filter__sideCategoryTitle}>Price</h4>
                 <div className={styles.filter__sideItem}>
                   <label htmlFor="min">
@@ -99,11 +132,61 @@ function Filter() {
                     Max
                   </label>
                 </div>
+                <h4 className={styles.filter__sideCategoryTitle}>Author</h4>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="andy">
+                    <input type="checkbox" id="andy" name="andy" />
+                    Andy
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="elnafrederick">
+                    <input type="checkbox" id="elnafrederick" name="elnafrederick" />
+                    Elnafrederick
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="randomdash">
+                    <input type="checkbox" id="randomdash" name="randomdash" />
+                    Randomdash
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="travis">
+                    <input type="checkbox" id="travis" name="travis" />
+                    Travis
+                  </label>
+                </div>
+                <h4 className={styles.filter__sideCategoryTitle}>Collection</h4>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="animals">
+                    <input type="checkbox" id="animals" name="animals" />
+                    Animals
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="future">
+                    <input type="checkbox" id="future" name="future" />
+                    Future
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="nature">
+                    <input type="checkbox" id="nature" name="nature" />
+                    Nature
+                  </label>
+                </div>
+                <div className={styles.filter__sideItem}>
+                  <label htmlFor="space">
+                    <input type="checkbox" id="space" name="space" />
+                    Space
+                  </label>
+                </div>
               </div>
               <div className={styles.filter__sideFooter}>
-                <button className={styles.filter__button} type="button">Apply</button>
+                <button className={styles.filter__applyBtn} type="button">Apply</button>
               </div>
-            </div>
+            </aside>
           </div>
         </div>
       </div>
