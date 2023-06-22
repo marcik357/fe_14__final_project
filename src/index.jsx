4d1460d9dd02de53b2e0139bdd51c1361e61de06
Nataliia Vorobei <natalia.vorobei27@gmail.com>
2023-06-22 10:39:12 +0300

    FIX: provider

--- src/index.jsx
+++ src/index.jsx
@@ -7,9 +7,11 @@ import {store} from './redux/store/index';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </Provider>,
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>,
 );
