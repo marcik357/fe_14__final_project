8534f763bf8237c06075244ed9b0fd00186eecbf
Артём Ситников <sitnikov.artem91@gmail.com>
2023-06-22 17:11:46 +0300

    ADD: add folder with mock data

---
+++ src/components/Basket/data/data.js
@@ -0,0 +1,34 @@
+export const data = [
+  {
+    id: '1',
+    src: '../img/item.png',
+    title: 'Nft',
+    text: 'Price',
+    quantity: 2,
+    price: 0.25,
+  },
+  {
+    id: 2,
+    src: '../img/item2.png',
+    title: 'Nft',
+    text: 'Price',
+    quantity: 1,
+    price: 0.17,
+  },
+  {
+    id: 3,
+    src: '../img/Frame.png',
+    title: 'Nft',
+    text: 'Price',
+    quantity: 5,
+    price: 0.13,
+  },
+  {
+    id: '4',
+    src: '../img/Frame2.png',
+    title: 'Nft',
+    text: 'Price',
+    quantity: 1,
+    price: 0.07,
+  },
+];
