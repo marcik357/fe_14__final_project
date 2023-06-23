df6202ba98f4bcdea513c4c24c500835f93fa6b6
Артём Ситников <sitnikov.artem91@gmail.com>
2023-06-23 18:22:01 +0300

    before merge

--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-// import style from './footer.module.scss'
-
-function Footer() {
-  return (
-    <footer>Footer</footer>
-  );
-}
-
-export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,1 @@
-export * from './MainLayout';
-export * from './Home';
 export * from './Cart';
-export * from './Product';
-export * from './Categories';
-export * from './NotFound';
