bffa6b799783db31b72e9d99491f84304b0fac05
marcik357 <marcik357@gmail.com>
2023-06-25 15:22:39 +0300

    UPDATE: replace mock data with database; minor style changes

--- src/components/ProductCard/index.jsx
+++ src/components/ProductCard/index.jsx
@@ -4,18 +4,20 @@ import styles from './productCard.module.scss';
 import { buyNowHandler } from '../../utils';
 import { useDispatch } from 'react-redux';
 import { Link } from 'react-router-dom';
+import { Verified } from '../Icons/verified';
+import { ETHIcon } from '../Icons';
 
-function ProductCard({ url, userIcon, creator, verifiedIcon, price, name, id }) {
+function ProductCard({ imageUrls, userIcon, author, currentPrice, name, itemNo }) {
   const dispatch = useDispatch()
 
   return (
     <div className={styles.productCard}>
-      <Link to={`/product/${id}`}>
-      <img
-        className={styles.productCard__img}
-        src={url}
-        alt='product-card'
-      />
+      <Link to={`/product/${itemNo}`}>
+        <img
+          className={styles.productCard__img}
+          src={imageUrls[0]}
+          alt='product-card'
+        />
       </Link>
       <div className={styles.productCard__userInfo}>
         <div className={styles.productCard__userInfo_items}>
@@ -25,43 +27,24 @@ function ProductCard({ url, userIcon, creator, verifiedIcon, price, name, id }) 
             src={userIcon}
             alt='user-avatar'
           />
-          <p className={styles.productCard__userInfo_creator}>@{creator}</p>
+          <p className={styles.productCard__userInfo_author}>{author}</p>
         </div>
-
-        <img
-          className={styles.productCard__userInfo_verifiedIcon}
-          src={verifiedIcon}
-          alt='verified-icon'
-        />
+        <Verified />
       </div>
       <div className={styles.productCard__priceInfo}>
         <div className={styles.productCard__priceInfo_price}>
           <button
             className={styles.productCard__priceInfo_button}
             type='button'
-            onClick={() => buyNowHandler(dispatch, id)}
+            onClick={() => buyNowHandler(dispatch, itemNo)}
           >
             Buy now
           </button>
         </div>
         <div className={styles.productCard__priceInfo_buyNow}>
-          <svg
-            fill='#000000'
-            viewBox='0 0 32 32'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <g id='SVGRepo_bgCarrier' strokeWidth='0' />
-            <g
-              id='SVGRepo_tracerCarrier'
-              strokeLinecap='round'
-              strokeLinejoin='round'
-            />
-            <g id='SVGRepo_iconCarrier'>
-              <path d='M15.927 23.959l-9.823-5.797 9.817 13.839 9.828-13.839-9.828 5.797zM16.073 0l-9.819 16.297 9.819 5.807 9.823-5.801z' />
-            </g>
-          </svg>
+          <ETHIcon />
           <p>
-            {price}
+            {currentPrice}
             &nbsp;
             <span>ETH</span>
           </p>
@@ -72,19 +55,17 @@ function ProductCard({ url, userIcon, creator, verifiedIcon, price, name, id }) 
 }
 
 ProductCard.propTypes = {
-  url: PropTypes.string,
+  imageUrls: PropTypes.array,
   userIcon: PropTypes.string,
-  creator: PropTypes.string,
-  verifiedIcon: PropTypes.string,
-  price: PropTypes.string,
+  author: PropTypes.string,
+  currentPrice: PropTypes.number,
 };
 
 ProductCard.defaultProps = {
-  url: '',
-  userIcon: '',
-  creator: '',
-  verifiedIcon: '',
-  price: 0,
+  imageUrls: [],
+  userIcon: './images/avatars/user-icon.png',
+  author: 'varios author',
+  currentPrice: 0,
 };
 
 export default ProductCard;
