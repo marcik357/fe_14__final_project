deeec2821904f30c45c26171e9a174096b5e1313
Yan-Yaremenko <luxray0@gmail.com>
2023-06-26 19:27:16 +0300

    FIX: productCard styles

--- src/components/Icons/arrow-right.jsx
+++ src/components/Icons/arrow-right.jsx
@@ -1,11 +1,10 @@
 import PropTypes from 'prop-types';
 
-export function ArrowRight({ width, height, color, style }) {
+export function ArrowRight({ width, height, color }) {
   return (
     <svg
       width={width}
       height={height}
-      style={style}
       viewBox='0 0 24 24'
       fill='none'
       xmlns='http://www.w3.org/2000/svg'
