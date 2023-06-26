931b1637afe03596a4cfd0249b5c3ff059434683
Олександр Поплавський <o.poplavskiy@agro.corp>
2023-06-26 15:59:54 +0300

    FIX: main picture

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,15 +1,22 @@
+
+import { Link } from 'react-router-dom';
 import styles from './blog.module.scss';
 import postsData from '../../data.json';
-// import {Link} from "react-router-dom";
-import MainImg from '../../img/img-blog-main.png';
+import MainImg from '../../img/MainPicture.jpg';
 import { BlogIcon } from '../Icons/icon-blog';
 
 export default function Blog() {
   return (
     <div className={styles.blog}>
 
-      <div className={styles.blog__mainImg}>
-        <img src={MainImg} alt="img" />
+      <div className={styles.blog__mainImgContainer}>
+        <div className={styles.blog__mainImg}>
+          <img src={MainImg} alt="img" />
+        </div>
+        <div className={styles.blog__titleOverlay}>
+          <h2>Crypter Blog</h2>
+          <p>Crypter NFT marketplace blog</p>
+        </div>
       </div>
       <div className={styles.blog__posts}>
         {
@@ -19,15 +26,18 @@ export default function Blog() {
                 <div className={`${styles.blog__post} ${index % 2 !== 0 ? styles.blog__reverse : ''}`}>
                   <div className={styles.blog__postImgBlock}>
                     <img className={styles.blog__postImg} src={data.url} alt={`img ${data.id}`} />
-                    <button type="button" className={styles.blog__btnImg}>{data.textButton}</button>
+                    <Link to="*">
+                      <button type="button" className={styles.blog__btnImg}>{data.textButton}</button>
+                    </Link>
                   </div>
                   <div className={styles.blog__contentBlock}>
                     <div className={styles.blog__content}>
-                      <h3 className={styles.blog__capturePost}>{data.capturePost}</h3>
+                      <Link to="*" className={styles.blog__linkCapturePost}>
+                        <h3 className={styles.blog__capturePost}>{data.capturePost}</h3>
+                      </Link>
                       <div className={styles.blog__infoAuthor}>
                         <div className={styles.blog__postIcon}>
                           <BlogIcon />
-                          {/* <img src={data.iconPost} alt={`icon ${data.id}`} /> */}
                         </div>
                         <div>
                           <p>{data.authorPost}</p>
@@ -43,7 +53,6 @@ export default function Blog() {
           })
         }
       </div>
-
     </div>
   );
 }
