8707023dfdf72779aae0d818171b00d7cc0b09e2
marcik357 <marcik357@gmail.com>
2023-06-27 18:50:53 +0300

    UPDATE: move fav-btn

--- src/components/ProductDetails/index.jsx
+++ src/components/ProductDetails/index.jsx
@@ -2,10 +2,9 @@ import PropTypes from 'prop-types'; 3
 import { useDispatch } from 'react-redux';
 import { buyNowHandler } from '../../utils';
 import style from './productDetails.module.scss';
-// потім перейменувати у style
 import { ETHIcon } from '../Icons';
 
-export default function ProductDetails({ _id, itemNo, name, currentPrice, imageUrls, authorIcon, collectionIcon, author, theme }) {
+export default function ProductDetails({ _id, itemNo, name, currentPrice, imageUrls, authorIcon, collectionIcon, author, theme, details }) {
   const dispatch = useDispatch();
 
   return (
@@ -33,6 +32,7 @@ export default function ProductDetails({ _id, itemNo, name, currentPrice, imageU
       <div className={style.productDetails__actions}>
         <div className={style.productDetails__actions_header}>
           <h2 className={style.productDetails__actions_title}>{name}</h2>
+          <button className={style.productDetails__buttons_fav}>♡</button>
         </div>
         <div className={style.productDetails__actions_price}>
           <p>Price</p>
@@ -44,16 +44,7 @@ export default function ProductDetails({ _id, itemNo, name, currentPrice, imageU
         </div>
         <div className={style.productDetails__descr}>
           <h3 className={style.productDetails__descr_title}>Details:</h3>
-          <p className={style.productDetails__descr_text}>We are laying the groundwork for web3 — the next generation of the internet full of limitless possibilities. Join the millions of creators, collectors, and curators who are on this journey with you.</p>
-        </div>
-        <div className={style.productDetails__buttons}>
-          <div className={style.productDetails__buttons_container}>
-            <div className={style.productDetails__buttons_actions}>
-              {/* <button className={style.productDetails__buttons_buy} onClick={() => buyNowHandler(dispatch, itemNo)}>Buy now</button> */}
-              <button className={style.productDetails__buttons_fav}>♡</button>
-              {/* <button className={style.productDetails__buttons_delete}>Delete</button> */}
-            </div>
-          </div>
+          <p className={style.productDetails__descr_text}>{details}</p>
         </div>
         <button className={style.productDetails__actions_mainBtn} onClick={() => buyNowHandler(dispatch, itemNo)}>Buy now</button>
         <p className={style.productDetails__actions_text}>We are laying the groundwork for web3 — the next generation of the internet full of limitless possibilities. Join the millions of creators, collectors, and curators who are on this journey with you.</p>
