1d5f206a0bfc6c86310cf869cddcaa74caf2f924
poplavskyi <76044968+poplavskyi@users.noreply.github.com>
2023-06-27 20:24:51 +0300

    Blog (#21)
    * CHANGE: index.jsx, blog.module.scss
    
    * FIX: img number 2
    
    * ADD: styles.blog__reverse
    
    * FIX: errors eslint
    
    * ADD: font
    
    * ADD: component BlogIcon
    
    * FIX: main picture
    
    * ADD: adaptive 375px
    
    * ADD: adaptive 1024px
    
    * FIX: adaptive 1024px
    
    * ADD: adaptive 1440px
    
    ---------
    
    Co-authored-by: Олександр Поплавський <o.poplavskiy@agro.corp>
    Co-authored-by: marcik357 <45140641+marcik357@users.noreply.github.com>

--- src/components/Blog/index.jsx
+++ src/components/Blog/index.jsx
@@ -1,48 +1,56 @@
-import styles from "./blog.module.scss"
-import Data from '../../data.json';
-// import {Link} from "react-router-dom";
-import MainImg from '../../img/img-blog-main.png'
+import { Link } from 'react-router-dom';
+import styles from './blog.module.scss';
+import postsData from '../../data.json';
+import MainImg from '../../img/MainPicture.jpg';
+import { BlogIcon } from '../Icons/icon-blog';
 
 export default function BlogComp() {
 
   return (
-    <div className={styles.blog_home}>
-
-      <div className={styles.blog_main}>
-        <img src={MainImg} alt=""></img>
+    <div className={styles.blog}>
+      <div className={styles.blog__mainImgContainer}>
+        <div className={styles.blog__mainImg}>
+          <img src={MainImg} alt="img" />
+        </div>
+        <div className={styles.blog__titleOverlay}>
+          <h2>Crypter Blog</h2>
+          <p>Crypter NFT marketplace blog</p>
+        </div>
       </div>
-      
-      <div className={styles.blog_posts}>
-        {
-          Data.map( data => {
+      <div className={styles.blog__posts}>
+        {postsData.map((data, index) => {
             return (
-              <div>
-                <div className={styles.blog_post} key={data.id}>
-                  <div className={styles.blog_postImg}>
-                    <img src={data.url} alt="img"/>
+              <div key={data.id}>
+                <div className={`${styles.blog__post} ${index % 2 !== 0 ? styles.blog__reverse : ''}`}>
+                  <div className={styles.blog__postImgBlock}>
+                    <img className={styles.blog__postImg} src={data.url} alt={`img ${data.id}`} />
+                    <Link to="*">
+                      <button type="button" className={styles.blog__btnImg}>{data.textButton}</button>
+                    </Link>
                   </div>
-                  <div className={styles.blog_content}>
-                    <h3>{data.capturePost}</h3>
-                    <div className={styles.blog_infoAuthor}>
-                      <div className={styles.blog_postIcon}>
-                        <img src={data.iconPost} alt=""/>
+                  <div className={styles.blog__contentBlock}>
+                    <div className={styles.blog__content}>
+                      <Link to="*" className={styles.blog__linkCapturePost}>
+                        <h3 className={styles.blog__capturePost}>{data.capturePost}</h3>
+                      </Link>
+                      <div className={styles.blog__infoAuthor}>
+                        <div className={styles.blog__postIcon}>
+                          <BlogIcon />
+                        </div>
+                        <div>
+                          <p className={styles.blog__authorPost}>{data.authorPost}</p>
+                          <p className={styles.blog__dataPost}>{data.dataPost}</p>
+                        </div>
                       </div>
-                      <div>
-                        <p>{data.authorPost}</p>
-                        <p>{data.dataPost}</p>
-                      </div>
+                      <p className={styles.blog__textPost}>{data.textPost}</p>
                     </div>
-                    <p>{data.textPost}</p>
-                    <button>{data.textButton}</button>
                   </div>
                 </div>
               </div>
-            )
+            );
           })
         }
       </div>
-      
-
     </div>
-  )
+  );
 }
