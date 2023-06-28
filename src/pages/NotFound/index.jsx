f691dfc5f23281cf56024d8613f1c809911e53cf
Nataliia Vorobei <natalia.vorobei27@gmail.com>
2023-06-29 00:13:58 +0300

    FIX: links

--- src/pages/NotFound/index.jsx
+++ src/pages/NotFound/index.jsx
@@ -1,9 +1,25 @@
-import PageNotFound from "../../components/404";
+import { NavLink } from 'react-router-dom';
+import style from './notFound.module.scss';
 
 export function NotFound() {
   return (
-    <div>
-      <PageNotFound />
-    </div>
+    <>
+      <div className={style.notFound}>
+        <div className={style.notFound__container}>
+          <h1 className={style.notFound__container_title}>404</h1>
+          <h2 className={style.notFound__container_subtitle}>Oops, this page not found!</h2>
+          <p className={style.notFound__container_info}>The link might be corrupted.</p>
+          <p className={style.notFound__container_subinfo}>or the page may have been removed</p>
+          <div className={style.notFound__buttons}>
+            <NavLink to={-1} className={style.notFound__btn}>
+              Previous page
+            </NavLink>
+            <NavLink to="/" className={style.notFound__btn}>
+              Go to home
+            </NavLink>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
