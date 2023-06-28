590b2702957e2ce3dd1071cdf54bd8a911fe576d
Slava Pivovarov <sllawwkoo@gmail.com>
2023-06-28 22:11:57 +0300

    CREATE: icon components account and login

--- src/components/Icons/index.js
+++ src/components/Icons/index.js
@@ -7,3 +7,5 @@ export { Facebook } from './facebook';
 export { Twitter } from './twitter';
 export { Close } from './close';
 export { ArrowRight } from './arrow-right';
+export { LogIn } from './login';
+export { Account } from './account';
