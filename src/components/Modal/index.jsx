614f722994d080b71e61adda8b0d723847a7c969
NatVorobei <113465891+NatVorobei@users.noreply.github.com>
2023-06-28 18:13:58 +0300

    Modal (#22)
    * FIX: modal overlay
    
    * ADD: new modal props
    
    * ADD: modal icon
    
    * ADD: modal-icon
    
    * REMOVE: modal close btn

--- src/components/Modal/index.jsx
+++ src/components/Modal/index.jsx
@@ -24,7 +24,6 @@ export function Modal(props) {
         <div className={style.modal__container}>
           <div className={style.modal__header}>
             <h3 className={style.modal__title}>{header}</h3>
-            <button type="button" className={style.modal__closeBtn} onClick={onCloseModal}>✕</button>
           </div>
           <div className={style.modal__text}>
             <div className={style.modal__icon}>
