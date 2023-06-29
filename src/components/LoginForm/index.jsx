f85ef7afc331fd835b55ddff060482b92e4fbfdd
Lilia Chekh <liliacheh@gmail.com>
2023-06-29 17:40:39 +0200

    CHANGED : exported the logInFormFields to a separate file

--- src/components/LoginForm/index.jsx
+++ src/components/LoginForm/index.jsx
@@ -6,35 +6,36 @@ import { login } from '../../utils';
 import { useDispatch } from 'react-redux';
 import { setModalType } from '../../redux/actions/modalActions';
 import { setErrorAction } from '../../redux/actions/errorActions';
+import { logInFormFields } from './logInFormFields';
 
 
 export default function LoginForm() {
   const dispatch = useDispatch()
   const baseUrl = 'https://plankton-app-6vr5h.ondigitalocean.app/api/'
-    const logInFormFields = [
-        {
-          tagType: 'regular',
-          label: "Login or Email",
-          labelClass: style.form__label,
-          inputClass: style.form__input,
-          errorClass: style.form__error,
-          id: "loginOrEmail",
-          name: "loginOrEmail",
-          type: "text",
-          placeholder: "Enter your login or email",
-        },
-        {
-          tagType: 'regular',
-          label: "Password",
-          labelClass: style.form__label,
-          inputClass: style.form__input,
-          errorClass: style.form__error,
-          id: "password",
-          name: "password",
-          type: "password",
-          placeholder: "Enter your password",
-        }
-      ];
+    // const logInFormFields = [
+    //     {
+    //       tagType: 'regular',
+    //       label: "Login or Email",
+    //       labelClass: style.form__label,
+    //       inputClass: style.form__input,
+    //       errorClass: style.form__error,
+    //       id: "loginOrEmail",
+    //       name: "loginOrEmail",
+    //       type: "text",
+    //       placeholder: "Enter your login or email",
+    //     },
+    //     {
+    //       tagType: 'regular',
+    //       label: "Password",
+    //       labelClass: style.form__label,
+    //       inputClass: style.form__input,
+    //       errorClass: style.form__error,
+    //       id: "password",
+    //       name: "password",
+    //       type: "password",
+    //       placeholder: "Enter your password",
+    //     }
+    //   ];
   return (
     <Formik
       initialValues={{
