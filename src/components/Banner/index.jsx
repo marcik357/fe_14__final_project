0354542c8a5305dd5ccaeaf5d966c15ec6ebe1c8
marcik357 <marcik357@gmail.com>
2023-06-30 18:34:47 +0300

    UPDATE: banner minor changes

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -6,12 +6,12 @@ export default function Banner({ title, subtitle, img }) {
     <div className={styles.banner}>
       <img className={styles.banner__img} src={img} alt={title} />
       <div className={styles.banner__container}>
-        <h2 className={styles.banner__title}>
+        {title && <h2 className={styles.banner__title}>
           {title}
-        </h2>
-        <h3 className={styles.banner__subtitle}>
+        </h2>}
+        {subtitle && <h3 className={styles.banner__subtitle}>
           {subtitle}
-        </h3>
+        </h3>}
       </div>
     </div>
   );
