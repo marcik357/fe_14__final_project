0754418fe1783b0b30fa333e6dc7b2efbe299fab
Артём Ситников <sitnikov.artem91@gmail.com>
2023-06-30 10:23:03 +0300

    CHANGE: change name of reduser property

--- src/redux/reducers/cartReducer.js
+++ src/redux/reducers/cartReducer.js
@@ -2,17 +2,16 @@ import { cartTypes } from "../types/cartTypes";
 import { parseLocalStorageItem } from "../../components/Cart/LocalStorage";
 
 const initialState = {
-    cartProductsAdd:parseLocalStorageItem('cartProductsAdd') || [],
+    cartProductsArray:parseLocalStorageItem('cartProductsArray') || [],
     products:parseLocalStorageItem('products') || []
 }
 
-// cartProductsAdd:[...state.cartProductsAdd,action.payload.cartProductsAdd]
 export function cartReducer(state=initialState, action) {
     switch (action.type) {
-        case cartTypes.ADD_TO_CART:
+        case cartTypes.ADD_TO_CART_QUANTITY:
            return {...state,products:[...state.products,action.payload]}
         case cartTypes.ADD_TO_CART_PRODUCT:
-            return {...state,cartProductsAdd:[...state.cartProductsAdd,action.payload.cartProductsAdd]}
+            return {...state,cartProductsArray:[...state.cartProductsArray,action.payload.cartProductsArray]}
             case cartTypes.CHANGE_CART__PRODUCT:
                 return {...state,products:action.payload}
                 case cartTypes.LOAD_CART:
