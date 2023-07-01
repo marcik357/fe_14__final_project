7022507085c97853d77d2ec26e7cbffefc2526b8
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-01 19:26:35 +0300

    ADD:add cart to redux

--- src/redux/reducers/rootReducer.js
+++ src/redux/reducers/rootReducer.js
@@ -4,11 +4,8 @@ import { productsReducer as products } from './productsReducer';
 import { loadingReducer as loading } from './loadingReducer';
 import { errorReducer as error } from './errorReducer';;
 import { modalReducer as modal } from './modalReducer';
-<<<<<<<<< Temporary merge branch 1
 import { tokenReducer as token } from './tokenReducer';
-=========
 import { cartReducer as cart } from './cartReducer';
->>>>>>>>> Temporary merge branch 2
 
 export const rootReducer = combineReducers({
   artNum,
@@ -16,9 +13,6 @@ export const rootReducer = combineReducers({
   loading,
   error,
   modal,
-<<<<<<<<< Temporary merge branch 1
-  token
-=========
+  token,
   cart
->>>>>>>>> Temporary merge branch 2
 });
