149754c6d6ae187f54bb0681a5cbadd66be84ea3
Slava Pivovarov <sllawwkoo@gmail.com>
2023-07-03 00:40:06 +0300

    UPDATE:minor changes in component Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,25 +1,50 @@
-import style from './footer.module.scss'
+import { Link } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Logo, Search, Basket, LogIn, Instagram } from '../Icons';
+import style from './footer.module.scss';
+import socialData from '../SocialLink/socialData';
+import SocialLink from '../SocialLink';
+import menuData from '../MenuLink/menuData';
+import MenuLink from '../MenuLink';
 
 function Footer() {
+  // логінізація тимчасова!!!!!
+  const [isLogin, setIsLogin] = useState(false);
+
   return (
     <footer className={style.footer}>
       <div className={style.footer__container}>
-         <div className={footer__section}>
-           <Link to="/">
-             <div className={style.footer_logo}>
-               <Logo />
-               <span className={style.footer__title}>CRYPTER</span>
-             </div>
+         <div className={style.footer__section}>
+           <Link to="/" className={style.footer__logo}>
+              <Logo width={48} height={48} color={'#202025'} />
+              <span className={style.footer__title}>CRYPTER</span>
            </Link>
-           <div className="footer__subtitle">
+           <div className={style.footer__subtitle}>
              Empower your creativity.
            </div>
          </div>
-         <nav className={`${style.footer__menu} ${menu}`}>
+         <nav className={`${style.footer__menu} ${style.menu}`}>
            <div className={`${style.menu__info}  ${style.info}`}>
              <h3 className={style.info__title}>INFO</h3>
              <ul className={style.info__list}>
-                <li className={style.info__item}>
+                {menuData.map(({type, page, text, icon, classHover}) => (
+                  (isLogin && type !== 'login') || (!isLogin && type !== 'account') ? (
+                    <MenuLink
+                      key={type}
+                      classItem={style.info__item}
+                     //  classHover={style[classHover]}
+                      page={page}
+                     //  isActive={isActive(page)}
+                     //  classActive={style.activeLink}
+                     //  closeBurgerMenu={() => toggleBurgerMenu()}
+                      text={text}
+                     //  isDesktop={isDesktop}
+                      isLogin={isLogin}
+                     //  icon={icon}
+                  />
+                  ) : null
+                  ))}
+                {/* <li className={style.info__item}>
                   <Link to="/categories">
                     <span>Store</span>
                   </Link>
@@ -38,13 +63,23 @@ function Footer() {
                   <Link to="*">
                     <span>Shopping cart</span>
                   </Link>
-                </li>
+                </li> */}
               </ul>
            </div>
-           <div className={`${style.menu__soc} ${style.soc}`}>
-             <h3 className={style.soc__title}>SOCIAL</h3>
+           <div className={`${style.menu__soc} ${style.social}`}>
+             <h3 className={style.social__title}>SOCIAL</h3>
              <ul className={style.social__list}>
-                <li className={style.social__item}>
+                {socialData.map(({type, url, icon}) => (
+                    <SocialLink
+                      key={type}
+                      classLi={style.social__item}
+                      url={url}
+                      classUrl={style.social__link}
+                      icon={icon('#686A6C')}
+                      text={type}
+                  />
+                  ))}
+                {/* <li className={style.social__item}>
                   <a href="https://www.instagram.com/nft_community/" className={style.social__link} target="_blank" rel="noreferrer">
                     <Instagram color="#010101" />
                   </a>
@@ -63,10 +98,13 @@ function Footer() {
                   <a href="https://www.linkedin.com/groups/13992662/" className={style.social__link} target="_blank" rel="noreferrer">
                     <Linkedin color="#010101" />
                   </a>
-                </li>
+                </li> */}
               </ul>
            </div>
          </nav>
+         <div className={style.footer__info}>
+           Copyright © 2022 UI8 LLC. All rights reserved
+         </div>
       </div>
     </footer>
   );
