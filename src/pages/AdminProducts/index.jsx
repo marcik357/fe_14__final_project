dec72f2af7f043ab649ab3b97bf1286f6bf73c99
Lilia Chekh <liliacheh@gmail.com>
2023-07-04 13:52:00 +0200

    CHANGE: changed handleEditButtonClick

--- src/pages/AdminProducts/index.jsx
+++ src/pages/AdminProducts/index.jsx
@@ -3,28 +3,38 @@ import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { baseUrl } from '../../utils/vars';
 import { getDataAction } from '../../redux/actions/getDataActions';
-import { addProductsAction, addPromoAction } from '../../redux/actions/productsActions';
+import { addProductsAction} from '../../redux/actions/productsActions';
 import { setArtNumAction } from "../../redux/actions/artNumActions";
-
+import EditProductForm from "../../components/EditProductForm";
 
 export function AdminProducts(){
   
   const [openForm, setOpenForm] = useState(false);
+  const [productId, setProductId] = useState(null)
   const products = useSelector((state) => state.products.products);
   const dispatch = useDispatch();
 
+  const product = products.find((product) => product._id === productId)
+  
   function handleEditButtonClick(dispatch, _id) {
     dispatch(setArtNumAction(_id))
-    console.log(_id)
+    setProductId(_id)
     setOpenForm(true);
+  }
+  function handleFormClose() {
+    setOpenForm(false);
   }
   useEffect(() => {
     dispatch(getDataAction(`${baseUrl}products`, addProductsAction));
   }, [dispatch]);
-    return <> {openForm && <div> Form Open</div>}
-    <ProductList
+
+    return <>
+    {openForm ?
+    <EditProductForm product={product} onCloseForm={handleFormClose}/>
+    : <ProductList
     products={products}
     customButtonText="Edit"
-    customButtonHandler={(dispatch, _id) => handleEditButtonClick(dispatch, _id)}/>
+    customButtonHandler={(dispatch, _id) => handleEditButtonClick(dispatch, _id)}
+    />}
     </>
 }
