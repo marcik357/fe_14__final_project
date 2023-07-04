2a248cbc726649adfeab8795148bca0c99cccdae
Lilia Chekh <liliacheh@gmail.com>
2023-07-04 13:44:41 +0200

    ADD : add Select component

---
+++ src/components/Select/index.jsx
@@ -0,0 +1,34 @@
+import { useField } from 'formik';
+
+const Select = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label
+       htmlFor={props.id}
+       className={props.labelClass}>
+       {label}
+      </label>
+      <select
+        className={props.inputClass}
+        type={props.type}
+        placeholder={props.placeholder}
+        id={props.id}
+        {...field}
+        value={field.value || ''}
+        multiple = {props.multiple}
+      >
+        {props.options.map((option) => (
+          <option key={option} value={option}>
+            {option}
+          </option>
+        ))}
+      </select>
+      {meta.touched && meta.error ? (
+        <div className={props.errorClass}>{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+export default Select;
