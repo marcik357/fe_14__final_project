4df504854e119233d7e26a3bb70f8f3f6bddf4dd
Олександр Поплавський <o.poplavskiy@agro.corp>
2023-07-06 14:40:23 +0300

    ADD: module.scss

--- src/components/HelpCenter/index.jsx
+++ src/components/HelpCenter/index.jsx
@@ -18,76 +18,84 @@ export function HelpCenter() {
   };
 
   return (
-    <div>
-      <div className={styles.helpCenterCube}>
-        <div className={styles.helpCenterCube__blockCube}>
-          <h2>Gettings started on Crypter</h2>
-          <img src={cube} alt="cube" />
+    <div className={styles.helpCenter}>
+      <div className={styles.helpCenter__content}>
+        <div className={styles.helpCenter__cubeBlockWrapper}>
+          <div className={styles.helpCenter__cubeBlock}>
+            <h2>Gettings started on Crypter</h2>
+            <img src={cube} alt="cube" />
+          </div>
         </div>
-      </div>
-      <div>
-        <div>
-          <button
-            type="button"
-            className={`block-button ${
-              activeBlocks.includes(1) ? "active" : ""
-            }`}
-            onClick={() => toggleBlock(1)}
-          >
-            Buying
-            <span>{activeBlocks.includes(1) ? <CloseList /> : <OpenList />}</span>
-          </button>
-          {activeBlocks.includes(1) && (
-            <ul>
+        <div className={styles.helpCenter__infoBlock}>
+          <div>
+            <button
+              type="button"
+              className={`${styles.blockButton} ${
+                activeBlocks.includes(1) ? styles.active : ""
+              }`}
+              onClick={() => toggleBlock(1)}
+            >
+              Buying
+              <span>
+                {activeBlocks.includes(1) ? <CloseList /> : <OpenList />}
+              </span>
+            </button>
+            {activeBlocks.includes(1) && (
+              <ul>
                 <li>Connect your wallet to Crypter</li>
                 <li>Guide to collection NFT artworks on Crypter</li>
                 <li>Marketplace balance</li>
                 <li>Marketplace fees</li>
                 <li>What is offer price?</li>
-            </ul>
-          )}
-        </div>
-        <div>
-          <button
-            type="button"
-            className={`block-button ${
-              activeBlocks.includes(2) ? "active" : ""
-            }`}
-            onClick={() => toggleBlock(2)}
-          >
-            Selling
-            <span>{activeBlocks.includes(2) ? <CloseList /> : <OpenList />}</span>
-          </button>
-          {activeBlocks.includes(2) && (
-            <ul>
+              </ul>
+            )}
+          </div>
+          <div>
+            <button
+              type="button"
+              className={`${styles.blockButton} ${
+                activeBlocks.includes(2) ? styles.active : ""
+              }`}
+              onClick={() => toggleBlock(2)}
+            >
+              Selling
+              <span>
+                {activeBlocks.includes(2) ? <CloseList /> : <OpenList />}
+              </span>
+            </button>
+            {activeBlocks.includes(2) && (
+              <ul>
                 <li>Connect your wallet to Crypter</li>
                 <li>Guide to collection NFT artworks on Crypter</li>
                 <li>Marketplace balance</li>
                 <li>Marketplace fees</li>
                 <li>What is offer price?</li>
-            </ul>
-          )}
-        </div>
-        <div>
-          <button
-            type="button"
-            className={`block-button ${
-              activeBlocks.includes(3) ? "active" : ""
-            }`}
-            onClick={() => toggleBlock(3)}
-          >
-            Creating
-            <span>{activeBlocks.includes(3) ? <CloseList /> : <OpenList />}</span>
-          </button>
-          {activeBlocks.includes(3) && (
-            <ul>
+              </ul>
+            )}
+          </div>
+          <div>
+            <button
+              type="button"
+              className={`${styles.blockButton} ${
+                activeBlocks.includes(3) ? styles.active : ""
+              }`}
+              onClick={() => toggleBlock(3)}
+            >
+              Creating
+              <span>
+                {activeBlocks.includes(3) ? <CloseList /> : <OpenList />}
+              </span>
+            </button>
+            {activeBlocks.includes(3) && (
+              <ul>
                 <li>Connect your wallet to Crypter</li>
                 <li>Guide to collection NFT artworks on Crypter</li>
                 <li>Marketplace balance</li>
                 <li>Marketplace fees</li>
                 <li>What is offer price?</li>
-            </ul>
-          )}
+              </ul>
+            )}
+          </div>
         </div>
       </div>
     </div>
