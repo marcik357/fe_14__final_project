9dd9a66ecc02024839075fb948e371cb4f7ef0e2
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-08 17:28:23 +0300

    CHANGE:refactoring code,rename methods

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -3,64 +3,64 @@ import style from './index.module.scss';
 import { CartList } from '../../components/CartList';
 import { FormToBuy } from '../../components/FormToBuy';
 import { useSelector,useDispatch } from 'react-redux';
-import { reloadPageGetCart } from '../../redux/actions/cartActions';
 import Loader from '../../components/Loader';
-let token = localStorage.getItem('token');
-
-function sumOfOrder(cartProductsArray,products) {
-  let sumOfNftPrice
-  if(cartProductsArray.length === 0 && (products.length === 0 )) sumOfNftPrice = 0
-  else{
-    const arrayOfNftPrice = products.map((item) =>
-    {
-      return cartProductsArray?.find(elem=> elem._id === item.product).currentPrice * item.cartQuantity
-    }
-    );
-    sumOfNftPrice = arrayOfNftPrice?.reduce((sum, item) => sum + item, 0);
-  }
-  return sumOfNftPrice;
-}
+import { getDataAction } from '../../redux/actions/getDataActions';
+import { addProductsAction } from '../../redux/actions/productsActions';
+import { baseUrl } from '../../utils/vars';
 
 export function Cart() {
   const [orderAmount, setOrderAmount] = useState();
-  const { cartProductsArray, products } = useSelector(state => state.cart);
-  // const { token } = useSelector(state => state.token);
+  const cart = useSelector(state => state.cart);
+  const { products } = useSelector(state=>state.products);
+  const { token } = useSelector(state => state.token);
   const loading = useSelector((state) => state.loading.loading);
   const dispatch = useDispatch();
 
-  useEffect(()=>{
-      !token ? (
-        localStorage.setItem('cartProductsArray',JSON.stringify(cartProductsArray)),
-        localStorage.setItem('products',JSON.stringify(products))
-      ):"";
-  },[cartProductsArray,products])
+  useEffect(() => {
+    dispatch(getDataAction(`${baseUrl}products`, addProductsAction));
+  
+  }, [dispatch, token]);
 
   useEffect(()=>{
-      token ? (dispatch(reloadPageGetCart(token))):""
-  },[dispatch])
-
-
-  useEffect(()=>{
-    setOrderAmount(sumOfOrder(cartProductsArray,products))
-  },[cartProductsArray,products])
+  if(token){
+    const cost = cart?.products?.map(({product,cartQuantity})=> {return cartQuantity * product.currentPrice} );
+    setOrderAmount(cost?.reduce((sum,item)=> {return sum + item}, 0))
+  }
+  else{
+  if(products.length > 0){
+    const cost = cart?.map(({product,cartQuantity})=>{
+      const productPrice= products?.find((products)=>products._id === product).currentPrice;
+      return cartQuantity * productPrice
+     })
+     setOrderAmount(cost?.reduce((sum,item)=> {return sum + item}, 0))
+    }
+  } },[cart,token])
   
   return (
     !loading ?(
     <div className={style.cart}>
       <div className={style.cart__block}>
-     
         <div className={style.block__items}>
-          {
-            cartProductsArray?.length > 0 ? cartProductsArray?.map((item) => (
-            <CartList
-              key={item.itemNo}
-              sumOfOrder={sumOfOrder}
+          {  cart?.products?.length > 0 || cart?.length > 0 ?
+          <>
+          { token ?  cart?.products?.map(({product,cartQuantity}) =>{
+            return (<CartList
+              key={product.itemNo}
+              cartQuantity = {cartQuantity}
               setOrderAmount={setOrderAmount}
-              {...item}
+              {...product}/>)})
+          : cart?.map((product) => {
+            console.log(products);
+              const productObj = products?.find((item) => item._id === product.product);
+              console.log(productObj);
+              return (<CartList
+              // key={ind}
+              {...productObj}
             />
-            )) : <p className={style.block__noItems}>No items in the Cart</p>
-            }
-        </div>
+            )})}
+            </>
+            :<p className={style.block__noItems}>No items in the Cart</p>}
+            </div>
         <div className={style.block__actionToBuy}>
           <FormToBuy orderAmount={orderAmount} />
         </div>
