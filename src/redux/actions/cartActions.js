ef1c5ab1868d16633a7865bf5da01e80225ebeee
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-08 17:26:53 +0300

    CHANGE:refactoring code. Change actions,type,reducer of cart

--- src/redux/actions/cartActions.js
+++ src/redux/actions/cartActions.js
@@ -2,263 +2,148 @@ import { cartTypes } from "../types/cartTypes";
 import { setErrorAction } from "./errorActions";
 import { setLoadingAction } from "./loadingActions";
 import { baseUrl } from '../../utils/vars'
+import { parseLocalStorageItem } from "../../components/Cart/LocalStorage";
 
-///workin without server
-export function loadCart(initialState){
-    return {
-        type:cartTypes.LOAD_CART,
-        payload:initialState
-}
-}
-export function changeQuantityInCartLocal(id,quantity,array,operation){
-    const increaseCart={
-        products:[{
-            product:id,
-            cartQuantity:quantity + 1 ,
-        }]
-    }
-    const decreaseCart={
-        products:[{
-            product:id,
-            cartQuantity:quantity - 1,
-        }]
-    }
-    const changeProduct= array.filter(item=>item.product !== id);
-    let updateCartQuantity;
-    if(operation){
-        updateCartQuantity = increaseCart.products[0];
-    }
-    else{
-       updateCartQuantity = decreaseCart.products[0];
-    }
-    const changeProductArray =changeProduct.length > 0 ? [updateCartQuantity,...changeProduct]:[updateCartQuantity];
-    
-    return {
-        type:cartTypes.CHANGE_CART__PRODUCT,
-        payload:changeProductArray
-  }
-}
 
-////for server and local storage
-export function addToCartQuantity(id){
-    return {
-        type:cartTypes.ADD_TO_CART_QUANTITY,
-        payload:{
-            product:id,
-            cartQuantity:1,
-     }
-   }
-}
-
-export function changeCartQuantity(changeProductArray){
-        return {
-        type:cartTypes.CHANGE_CART__PRODUCT,
-        payload:changeProductArray
-  }
-}
-
-export function addToCartProduct(data){
-    return {
-        type:cartTypes.ADD_TO_CART_PRODUCT,
-        payload:{
-            cartProductsArray:data,
+export const handleError = (response, code) => {
+    if (response.status === code) {
+      throw new Error(response.status)
     }
   }
-}
-
-export function deleteFromCartProduct(id,cartProduct,product){
-    const newCartProduct = cartProduct.filter(product=> product._id !== id)
-    const newProduct = product.filter(product=> product.product !== id)
-    const deleteProductArray = {
-        cartProductsArray:newCartProduct,
-        products:newProduct
-    }
-    return {
-        type:cartTypes.DELETE_CART,
-        payload:deleteProductArray,
-  }
-}
-
-////working with server and token
-export function reloadPageGetCart(token){
-    return async function(dispatch){
-            try{
-                dispatch(setLoadingAction(true));
-                const getFetch = await fetch(`${baseUrl}cart`,{
-                    headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'}
-                  })
-                  const response = await getFetch.json();
-                    const initialState = !response ? {
-                        cartProductsArray:[],
-                        products:[]
-                        } :
-                            {
-                              cartProductsArray:response.products.map(product => product.product),
-                              products:response.products.map(item=> {
-                                  return { cartQuantity:item.cartQuantity,
-                                           product:item.product._id}
-                               })
-                          }
-                          dispatch(loadCart(initialState))
-                          dispatch(setLoadingAction(false));
-                          dispatch(setErrorAction(null));
-            }
-            catch(error){
-                dispatch(setLoadingAction(false));
-                dispatch(setErrorAction(error));
-            }
+export async function fetchData(url, reqBody) {
+    try {
+      const response = await fetch(url, reqBody);
+      if (!response.ok) {
+        handleError(response, 401);
+        const error = await response.json();
+        throw new Error(error.message);
       }
-}
+      return await response.json();
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+  // export async function serverCart(token,baseUrl) {
+  //   const cart = await fetchData(`${baseUrl}cart`,
+  //   {headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'}});
+  //   return cart
+  // }
 
-export function addNewProductToCart(id,token){
+export function setCart(cart){
+    return {
+        type:cartTypes.CHANGE__CART,
+        payload:cart || [],
+    }
+}
+export function getDataLS(token,cart){
+  return async function(dispatch){
+    try{
+      const newCart =  await fetchData(`${baseUrl}cart`,{
+        method:"POST",
+        headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
+        body:JSON.stringify({products:cart})
+      })
+    dispatch(setCart(newCart))
+    localStorage.setItem('cart','[]')
+    }
+    catch(error){
+      dispatch(setErrorAction(error))
+    }
+  }
+}
+export function reloadPageSV(token){
     return async function(dispatch){
-        try {
-            const sendFetch = await fetch(`${baseUrl}cart/${id}`,{
+      try{
+        const cart = await fetchData(`${baseUrl}cart`,
+        {headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'}});
+        dispatch(setCart(cart == null ?[]:cart))
+    }
+    catch(error){
+      dispatch(setErrorAction(error))
+    }
+  }
+ }
+export function addToCart(id,token){
+   return async function(dispatch){
+    try{
+      if(token){
+        const cart = await fetchData(`${baseUrl}cart/${id}`,{
             method:"PUT",
             headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
           })
-          dispatch(addToCartQuantity(id))
-        }
-        catch(error){
-            dispatch(setErrorAction(error));
-        }
-    }
-}
-
-export function createCartOnTheServerFirst(id,token){
-    const newCart = {
-        products:[{
-            product:id,
-            cartQuantity:1
-        }]
-    }
-    return async function(dispatch){
-        try{
-            dispatch(setLoadingAction(true));
-            const getFetch = await fetch(`${baseUrl}cart`,{
-                method:"POST",
-                headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
-                body:JSON.stringify(newCart),
-              })
-            dispatch(addToCartQuantity(id))
-            dispatch(setLoadingAction(false));
-            dispatch(setErrorAction(null));
-        }
-        catch(error){
-            dispatch(setLoadingAction(false));
-            dispatch(setErrorAction(error));
-        }
-}
-}
-
-export function changeQuantityInCartWithServer(id,quantity,array,token,operation){
-    const increaseCart={
-        products:[{
-            product:id,
-            cartQuantity:quantity + 1
-        }]
-    }
-    const decreaseCart={
-        products:[{
-            product:id,
-            cartQuantity:quantity - 1
-        }]
-    }
-    const changeProduct= array.filter(item=>item.product !== id)
-    let updateCartQuantity;
-    if(operation){
-        updateCartQuantity = increaseCart.products[0]
+          dispatch(setCart(cart))
     }
     else{
-        updateCartQuantity = decreaseCart.products[0]
+        const cart = [...parseLocalStorageItem('cart'),{ product:id,cartQuantity:1}]
+        localStorage.setItem("cart",JSON.stringify(cart));
+        dispatch(setCart(cart))
     }
-    const changeProductArray =changeProduct.length > 0 ? [updateCartQuantity,...changeProduct]:[updateCartQuantity]
-    const changeQuantityForServer = {
-        products:changeProductArray
-    }
-    return async function (dispatch){
-        try{
-            const sendFetch = await fetch(`${baseUrl}cart`,{
-                method:"PUT",
-                headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
-                body:JSON.stringify(changeQuantityForServer)
-              })
-            dispatch(changeCartQuantity(changeProductArray));
-          dispatch(setErrorAction(null));
-        }
-        catch(error){
-            dispatch(setErrorAction(error));
-        }
-        
-    }
-}
-
-export function deleteFromCartProductWithServer(id,cartProduct,product,token){
-  return async function(dispatch){
-    try{
-        dispatch(setLoadingAction(true));
-        const sendFetch = await fetch(`${baseUrl}cart/${id}`,{
-            method:"DELETE",
-            headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
-          })
-          dispatch(deleteFromCartProduct(id,cartProduct,product));
-          dispatch(setLoadingAction(false));
-          dispatch(setErrorAction(null));
-       
     }
     catch(error){
-        dispatch(setLoadingAction(false));
-        dispatch(setErrorAction(error));
+      dispatch(setErrorAction(error))
     }
+   }
+ }
+
+export function changeQuantityProduct(id,token,cart,flag) {
+  return async function(dispatch){
+    try{
+    if(token){
+     const newQuantity = cart?.products.map(({product,cartQuantity}) => {
+      if(product._id === id){
+        flag ? cartQuantity+=1 : cartQuantity-=1
+        return {product:product._id, cartQuantity:cartQuantity }
+      }
+      return {product:product._id, cartQuantity:cartQuantity }
+     });
+     const newCart =  await fetchData(`${baseUrl}cart`,{
+                        method:"PUT",
+                        headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
+                        body:JSON.stringify({products:newQuantity})
+                      })
+                    dispatch(setCart(newCart))
+    }
+    else{
+      const newCart =parseLocalStorageItem('cart').map(({product,cartQuantity})=>{
+        if(product === id) {
+          flag ? cartQuantity +=1 : cartQuantity-=1
+          return {product:product, cartQuantity:cartQuantity}
+        }
+        return {product:product, cartQuantity:cartQuantity}
+      })
+        localStorage.setItem("cart",JSON.stringify(newCart));
+        dispatch(setCart(newCart))
+      }
+    }
+      catch(error){
+        dispatch(setErrorAction(error))
+      }
   }
 }
 
-export function deleteAllCart(){
-    const deleteProductArray = {
-        cartProductsArray:[],
-        products:[]
-    }
-    return {
-        type:cartTypes.DELETE_CART,
-        payload:deleteProductArray,
-    }
-}
+ export function deleteCart(id,token){
+    return async function(dispatch){
+      try{
 
-export function buyProduct(token){
-   return async function(dispatch){
-    try{
-        dispatch(setLoadingAction(true));
-        const sendFetch = await fetch(`${baseUrl}cart`,{
-            method:"DELETE",
-            headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
-          })
-          dispatch(deleteAllCart());
-          dispatch(setLoadingAction(false));
-          dispatch(setErrorAction(null));
+        if(token){
+          dispatch(setLoadingAction(true))
+            const cart = await fetchData(`${baseUrl}cart/${id}`,{
+                method:"DELETE",
+                headers:{'Authorization':`Bearer ${token}`,'Content-Type':'application/json'},
+              })
+              dispatch(setCart(cart == null ? []: cart))
+              dispatch(setLoadingAction(false))
+        }
+        else{
+            const cart =parseLocalStorageItem('cart').filter(item => item.product !== id);
+            localStorage.setItem("cart",JSON.stringify(cart));
+            dispatch(setCart(cart == null ?[]: cart ))
+          
+        }
+      }
+      catch(error){
+        dispatch(setErrorAction(error))
+      }
+        
     }
-    catch(error){
-        dispatch(setLoadingAction(false));
-        dispatch(setErrorAction(error));
-    }
-    }
-}
-
-export async function addProductNftToCart(dispatch,cartProductsArray,products,idProduct,token,itemNo){
-    try{
-        dispatch(setLoadingAction(true));
-        const getFetch = await fetch(`${baseUrl}products/${itemNo}`)
-        const response = await getFetch.json();
-        token ?
-        ( cartProductsArray.length > 0 && products.length > 0 ?(
-          dispatch(addNewProductToCart(idProduct,token))
-        ):dispatch(createCartOnTheServerFirst(idProduct,token)))
-        :dispatch(addToCartQuantity(idProduct));
-        dispatch(addToCartProduct(response));
-        dispatch(setLoadingAction(false));
-        dispatch(setErrorAction(null));
-    }
-    catch(error){
-        dispatch(setLoadingAction(false));
-        dispatch(setErrorAction(error));
-    }
-    }
+ }
