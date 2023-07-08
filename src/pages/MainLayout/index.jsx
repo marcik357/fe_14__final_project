0bf2c900ac77de8f7933a1eebad50a0a4a5f6e59
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-08 17:29:03 +0300

    ADD:add logic for cart

--- src/pages/MainLayout/index.jsx
+++ src/pages/MainLayout/index.jsx
@@ -6,17 +6,54 @@ import Header from '../../components/Header';
 import { Modal } from '../../components/Modal/index';
 import { modalProps } from '../../components/Modal/modalProps';
 import { setTokenAction } from '../../redux/actions/tokenActions';
+import { reloadPageSV,setCart,getDataLS} from '../../redux/actions/cartActions';
+import { getDataAction } from '../../redux/actions/getDataActions';
+import { addProductsAction } from '../../redux/actions/productsActions';
+import { parseLocalStorageItem } from '../../components/Cart/LocalStorage';
+import { baseUrl } from '../../utils/vars';
+
 
 export function MainLayout() {
   const dispatch = useDispatch()
   const modalType = useSelector((state) => state.modal.modal);
-
+  const cart = useSelector(state => state.cart);
+  const { token } = useSelector(state => state.token);
+  const { products } = useSelector(state => state.products);
+  const error = useSelector((state) => state.error.error);
   useEffect(() => {
-    dispatch(setTokenAction(localStorage.getItem('token')));
+    if (error == 401) {
+      dispatch(setModalType('login'));
+      dispatch(setErrorAction(null));
+    }
     modalType
       ? document.body.style.overflow = 'hidden'
       : document.body.style.overflow = 'auto';
-  }, [modalType, dispatch]);
+  }, [token, error, modalType, dispatch])
+
+  useEffect(() => {
+    dispatch(getDataAction(`${baseUrl}products`, addProductsAction));
+    if (!token) {
+      dispatch(setTokenAction(localStorage.getItem('token')));
+      dispatch(setCart(parseLocalStorageItem('cart')))
+      localStorage.setItem('cart',JSON.stringify(cart))
+    } else {
+      dispatch(reloadPageSV(token));
+    }
+  }, [dispatch, token])
+
+  useEffect(() => {
+    if (cart.length === 0) dispatch(getDataLS(token, parseLocalStorageItem('cart')));
+  }, [cart, token, dispatch])
+
+  // useEffect(()=>{
+
+  //   // if(token){
+  //   //   if(parseLocalStorageItem('cart').length > 0 && (serverCart(token,baseUrl) === null))
+  //   //   dispatch(getDataLS(token,parseLocalStorageItem('cart')))
+  //   //   // localStorage.setItem('cart','[]')
+  //   // }
+  // }
+  // ,[dispatch,cart,token])
 
   return (
     <>
