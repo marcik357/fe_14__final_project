889aee50ff7f196ea293684da73441bfd1cd8d91
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-08 17:30:53 +0300

    CHANGE:refactoring code,rename methods

--- src/components/CartList/index.jsx
+++ src/components/CartList/index.jsx
@@ -1,33 +1,22 @@
 import PropTypes from 'prop-types';
 import style from './index.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { changeQuantityInCartWithServer,changeQuantityInCartLocal,deleteFromCartProduct,deleteFromCartProductWithServer} from '../../redux/actions/cartActions';
+import { deleteCart,changeQuantityProduct } from '../../redux/actions/cartActions';
 import { Link } from 'react-router-dom';
 import Loader from '../Loader';
-let token = localStorage.getItem('token');
-let flag;
+
 
 export function CartList(props) {
     const dispatch = useDispatch();
-    const { imageUrls, name, currentPrice, sumOfOrder, setOrderAmount, _id ,itemNo } = props;
-    const { cartProductsArray,products } = useSelector(state => state.cart);
+    const { imageUrls, name, currentPrice, _id ,itemNo, cartQuantity } = props;
+    const cart = useSelector(state => state.cart);
+    const { token } = useSelector(state => state.token);
+    const { products } = useSelector(state => state.products);
     const loading = useSelector((state) => state.loading.loading);
-    let currentQuantity = products.length > 0 ? (products.find(item=>item.product === _id))?.cartQuantity: 0;
-  function deleteProductFromCart() {
-    token ?
-    dispatch(deleteFromCartProductWithServer(_id,cartProductsArray,products,token)) :
-    dispatch(deleteFromCartProduct(_id,cartProductsArray,products));
-    setOrderAmount(0)
-  }
-  
-  function changeValueOfProductInCart(flag) {
-    token? (dispatch(changeQuantityInCartWithServer(_id,currentQuantity,products,token,flag)),setOrderAmount(sumOfOrder(cartProductsArray,products))):
-    dispatch(changeQuantityInCartLocal(_id,currentQuantity,products,flag));
-    setOrderAmount(sumOfOrder(cartProductsArray,products))
-  }
+    let currentQuantity = token ? cartQuantity :((cart?.find(item => item.product === _id)).cartQuantity);
   
 
-  return (!loading ?
+    return (!loading ?
     <div className={style.cartListItem}>
       <div className={style.cartListItem__icon}>
         <Link to={`/product/${itemNo}`}>
@@ -38,7 +27,7 @@ export function CartList(props) {
         />
         </Link>
       </div>
-      <div data-price={currentPrice} className={style.cartListItem__description}>
+      <div className={style.cartListItem__description}>
         <Link to={`/product/${itemNo}`}>
         <p className={style.description__title}>{name}</p>
         </Link>
@@ -54,28 +43,27 @@ export function CartList(props) {
         <button
           type="submit"
           className={style.quantity__btn}
-          onClick={currentQuantity > 1 ? (()=>changeValueOfProductInCart(false)) : null}
+          onClick={currentQuantity > 1 ?  ()=>(dispatch(changeQuantityProduct(_id,token,cart,false))): null}
         > -
         </button>
        <div
          className={style.quantity__value}
        >
-        {products.length > 0 ? currentQuantity: 1}
+      {cart?.length > 0 || cart?.products.length >0 ? currentQuantity: 0}
        </div>
         <button
           type="button"
           className={style.quantity__btn}
-          onClick={()=> changeValueOfProductInCart(true)}
+          onClick={()=> dispatch(changeQuantityProduct(_id,token,cart,true))}
         > +
         </button>
       </div>
       <button
         type="submit"
         className={style.cartListItem__btnDelete}
-        onClick={deleteProductFromCart}
+        onClick={()=>(dispatch(deleteCart(_id,token)))}
       >
-        Delete
-   
+        &times;
       </button>
     </div>
     : <Loader/>
@@ -85,8 +73,6 @@ CartList.propTypes = {
   imageUrls: PropTypes.array,
   name: PropTypes.string,
   currentPrice: PropTypes.number,
-  sumOfOrder: PropTypes.func,
-  setOrderAmount: PropTypes.func,
   id: PropTypes.number,
   idCode:PropTypes.number,
   _id:PropTypes.string,
@@ -95,8 +81,6 @@ CartList.defaultProps = {
   imageUrls: [],
   name: '',
   currentPrice: 0,
-  sumOfOrder: '',
-  setOrderAmount: '',
   id: 0,
   idCode:0,
   _id:"",
