3a31b79d57694271752ceaf76da50155607d9f69
marcik357 <marcik357@gmail.com>
2023-07-11 20:26:54 +0300

    UPDATE: minor changes + merge

--- src/components/Icons/open-list.jsx
+++ src/components/Icons/open-list.jsx
@@ -13,23 +13,23 @@ export function OpenList() {
           cy="16"
           r="12"
           stroke="#686A6C"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M10.668 16H21.3346"
           stroke="#686A6C"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
         <path
           d="M16 21.332L16 10.6654"
           stroke="#686A6C"
-          stroke-width="1.5"
-          stroke-linecap="round"
-          stroke-linejoin="round"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
         />
       </svg>
     </div>
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,5 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
-import { MainLayout, Home, Cart, NotFound, Product, Blog, Order, Author, Authorization, Discover } from '../pages';
+import { MainLayout, Home, Cart, NotFound, Product, Blog, Order, Author, Authorization, Discover, Help } from '../pages';
 
 export default function Router() {
   return (
@@ -10,6 +10,7 @@ export default function Router() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/order" element={<Order />} />
+        <Route path="/help" element={<Help />} />
         <Route path="/authorization" element={<Authorization />} />
         <Route path="/author/:authorId" element={<Author />} />
         <Route path="/product/:productId" element={<Product />} />
