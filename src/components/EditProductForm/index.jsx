6aedf6f729473d9cccee9ad3143a009236a2d540
Lilia Chekh <liliacheh@gmail.com>
2023-07-11 15:39:27 +0200

    CHANGED: changed select to checkboxes for theme

--- src/components/EditProductForm/index.jsx
+++ src/components/EditProductForm/index.jsx
@@ -9,12 +9,17 @@ import { Formik, Form } from 'formik';
 import { validationSchemaProduct } from '../../validation';
 import Select from '../Select';
 import { putData } from '../../utils';
-import { useNavigate } from 'react-router-dom';
-
+import { useState } from 'react';
+// import { useNavigate } from 'react-router-dom';
+import Checkbox from '../Checkbox';
 
 export default function EditProductForm({product, onCloseForm}){
     const dispatch = useDispatch();
+    const [selectedThemes, setSelectedThemes] = useState(product.theme || []);
     // const navigate = useNavigate();
+    // const handleCheckboxChange  = (value) => {
+    //   setSelectedThemes(value);
+    // };
     return(
      
         <Formik
@@ -49,6 +54,20 @@ export default function EditProductForm({product, onCloseForm}){
             return (
               <Select key={field.name} {...field}/>
             );
+          } else if (field.tagtype === 'checkbox'){
+            return (<>
+              <label className={field.labelclass}>{field.label}</label>
+              <div className={field.divclass}>
+        {field.options.map((option) => {
+          return (
+            <Checkbox key={option} children={option} value = {selectedThemes}
+            onChange = {(value)=> setSelectedThemes(value)}{...field}
+            />
+        )
+        })}
+    </div>
+        </>
+            )
           }
           return null;
         })}
