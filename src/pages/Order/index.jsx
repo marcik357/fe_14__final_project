b34ae53b705aba0aa5f190380621e9ad3528394d
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-13 22:08:02 +0300

    ADD:useEffect,context

--- src/pages/Order/index.jsx
+++ src/pages/Order/index.jsx
@@ -1,9 +1,7 @@
-import styles from '../../components/FormikForm/form.module.scss';
-import styling from './order.module.scss'
-import axios from 'axios';
+import styling from './order.module.scss';
 import Banner from "../../components/Banner";
 import { useSelector } from 'react-redux';
-import { useState,useContext} from "react";
+import { useState,useContext } from "react";
 import { baseUrl } from '../../utils/vars';
 import { contactForm } from "../../components/ContactForm/contactForm.js";
 import { ContactForm } from '../../components/ContactForm';
@@ -11,46 +9,70 @@ import { PaymentForm } from '../../components/PaymentForm';
 import { BuyInfo } from '../../components/BuyInfo';
 import { paymentForm }from '../../components/PaymentForm/paymentForm.js';
 import { Quantity } from '../../router';
+import { newOrder } from '../../utils/vars';
+import { fetchData } from '../../utils';
 
-export function Order(props) {
+export function Order() {
 const token = useSelector(state=> state.token.token);
 const cart = useSelector(state=> state.cart.cart)
+const { products } =useSelector(state=> state.products);
 const customer =useSelector(state=> state.cart.cart.customerId);
-const loading =useSelector(state=> state.loading);
 const [orderAmount]=useContext(Quantity)
 const [active, setActive] =useState(false);
 const [contactValue, setContactValue] =useState();
 
-const newOrder = {
-  customerId: `${customer?._id}`,
-  products: '',
-  paymentInfo: "Credit card",
-  email: `${contactValue?.email}`,////есть поле
-  mobile: `${contactValue?.telephone}`,///есть поле
-  totalSum:`${orderAmount}`,
-  letterSubject: "Thank you for order! You are welcome!",
-  letterHtml:
-    `<h1>Your order is placed. OrderNo is ${Date.now()}.</h1><p>Total sum of order is ${orderAmount}ETH </p>`
-};
-newOrder.products = cart?.products;
 
- async function getOrder(token){
-  axios.defaults.headers.post['Authorization'] = `Bearer ${token}`;
-
-    axios
-    .post(`${baseUrl}orders`,newOrder)
-    .then(orders => {
-     
-    })
-    .catch(err => {
-      /*Do something with error, e.g. show error to user*/
-    });
-  }
+if(token){
+  newOrder.products =cart.products;
+  newOrder.customerId = customer;
+  newOrder.name = contactValue?.name;
+  newOrder.email = contactValue?.email;
+  newOrder.mobile = contactValue?.telephone;
+  newOrder.card = contactValue?.cardNumber;
+  newOrder.totalSum = orderAmount;
+  newOrder.letterHtml =
+  `
+  <h1>${contactValue?.name},Your order is placed.&#9989;</h1> 
+  <p></p>Number of order <b>${Date.now()}</b></p>
+  <p>Total sum of order is ${Number(orderAmount).toFixed(2)}ETH </p>
+  <p>Money was debited from the card &#128179;</p>
+  <p>We will miss you&#128546;,come back soon </p>
+  <p>Thank you for order, your crypto&#128154;</p>
+  <br/>
+  <br/>
+  <p>Email &#128233 :crypter.mailer@gmail.com</p>
+  <a href="http://localhost:3000">OUR HOME PAGE</a>
+  `
+}
+else{
+  const newCartArray = cart?.products.map(({product,cartQuantity})=>{
+  const productInfo = products.find(item=> item._id === product);
+  return {product:productInfo,cartQuantity}})
+  newOrder.products = newCartArray;
+  newOrder.name = contactValue?.name;
+  newOrder.email = contactValue?.email;
+  newOrder.card = contactValue?.cardNumber;
+  newOrder.mobile = contactValue?.telephone;
+  newOrder.totalSum = orderAmount;
+  newOrder.letterHtml =
+  `
+  <h1>${contactValue?.name},Your order is placed.&#9989;</h1> 
+  <p></p>Number of order ${Date.now()}</p>
+  <p>Total sum of order is ${Number(orderAmount).toFixed(2)}ETH </p>
+  <p>Money was debited from the card &#128179;</p>
+  <p>We will miss you&#128546;,come back soon </p>
+  <p>Thank you for order, your crypto&#128154;</p>
+  <br/> <br/>
+  <p>Email &#128233 :crypter.mailer@gmail.com</p>
+  <a href="http://localhost:3000">Our home page</a>
+  `
+}
 
   return (
     <>
-     <Banner  title="You orders in one touch" img="/images/banners/order-banner3.png"/>
+     <Banner  title="You orders in one touch" img="/images/banners/order-banner.png"/>
         <div className={styling.order}>
+          <BuyInfo className={styling.order_block__buy_info} />
           <div className={styling.order__user_info}>
             <h2 className={styling.user_info__title} >Contact form</h2>
             <button
@@ -67,14 +89,16 @@ newOrder.products = cart?.products;
             /> :
             <>
             <PaymentForm
-            getOrder={getOrder}
+            getOrder={()=>fetchData(`${baseUrl}orders`,{
+              method: "POST",
+              headers: {'Content-Type': 'application/json'},
+              body:JSON.stringify(newOrder)
+            },)}
             paymentForm={paymentForm}
             setContactValue={setContactValue}
             />
             </>}
           </div>
-          <BuyInfo className={styling.order_block__buy_info} />
-        
           </div>
     </>
   );
