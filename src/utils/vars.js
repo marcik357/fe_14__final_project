ddf4c69d32c3ac8b3f5dddf873de4f5817f2ac81
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-13 22:03:17 +0300

    ADD:add varible of letter

--- src/utils/vars.js
+++ src/utils/vars.js
@@ -1,1 +1,6 @@
-export const baseUrl = 'https://plankton-app-6vr5h.ondigitalocean.app/api/'
+export const baseUrl = 'https://plankton-app-6vr5h.ondigitalocean.app/api/';
+export const newOrder ={
+      products: '',
+      paymentInfo: "Credit card",
+      letterSubject: "Thank you for order! You are welcome!",
+};
