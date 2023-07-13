8ccf956502a583540a884c224272e39b10ab3125
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-13 22:10:03 +0300

    CHANGE:change minor code

--- src/components/BuyInfo/index.jsx
+++ src/components/BuyInfo/index.jsx
@@ -4,21 +4,34 @@ import style from './index.module.scss';
 import { Quantity } from '../../router';
 
 export function BuyInfo (){
-const cart = useSelector(state=> state.cart.cart.products);
+const { token } = useSelector(state=> state.token);
+const cart = useSelector(state=> state.cart.cart);
+const { products } =useSelector(state=> state.products);
 const [ orderAmount ] = useContext(Quantity);
+
     return (
        <div className={style.cart_block}>
          <h1 className={style.cart_block__title}>Buy info</h1>
               <div className={style.cart_block__products}>
-                {cart?.map(({product,cartQuantity}) =>(
+                { token ? cart?.products?.map(({product,cartQuantity}) =>(
                     <div className={style.products__card} >
-                        <img className={style.card__img} src={product.imageUrls} alt={product.name}/>
-                    </div>
-                )  )}
+                      <img className={style.card__img} src={product.imageUrls} alt={product.name}/>
+                    </div>))
+                  : (cart?.products?.map(({product,cartQuantity})=>{
+                    const prod = products?.find((item)=>item._id === product)
+                    return (
+                      <div className={style.products__card} >
+                            <img className={style.card__img} src={prod?.imageUrls} alt={prod?.name}/>
+                      </div>
+                    )
+                  })
+                )}
               </div>
               <div className={style.cart_block__total} >
                 <p> Total Price :</p>
-                <span>{Number(orderAmount.toFixed(2)) || 0} ETH</span>
+                <span>
+                  {Number(orderAmount?.toFixed(2)) || 0}&nbsp;
+                  ETH</span>
               </div>
        </div>
     )
