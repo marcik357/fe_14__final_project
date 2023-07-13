f96aa7dec295837e86a1ccb1977a4bb6d5e7d455
Артём Ситников <sitnikov.artem91@gmail.com>
2023-07-13 22:07:25 +0300

    CHANGE:delete useEffect and made minor changes

--- src/pages/Cart/index.jsx
+++ src/pages/Cart/index.jsx
@@ -1,34 +1,15 @@
-import { useEffect, useContext } from 'react';
 import style from './index.module.scss';
 import { CartList } from '../../components/CartList';
 import { FormToBuy } from '../../components/FormToBuy';
 import { useSelector } from 'react-redux';
 import Loader from '../../components/Loader';
-import { Quantity } from '../../router';
 
 export function Cart() {
-  const [orderAmount, setOrderAmount] = useContext(Quantity);
   const token = useSelector(state => state.token.token);
   const loading = useSelector((state) => state.loading.loading);
   const cart = useSelector((state) => state.cart.cart);
   const products = useSelector((state) => state.products.products);
 
-  useEffect(() => {
-    if (token) {
-      const prices = cart?.products?.map(({ cartQuantity, product }) => {
-        return (cartQuantity * product.currentPrice)
-      })
-      setOrderAmount(prices?.reduce((prev, next) => prev + next, 0))
-    } else {
-      if (products.length > 0) {
-        const prices = cart?.products?.map(({ cartQuantity, product }) => {
-          const productR = products?.find((item) => item._id === product)
-          return (cartQuantity * productR.currentPrice)
-        })
-        setOrderAmount(prices?.reduce((prev, next) => prev + next, 0))
-      }
-    }
-  }, [cart, products, token])
 
   return (
     !loading ?
