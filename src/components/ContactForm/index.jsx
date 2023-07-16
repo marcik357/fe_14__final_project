b299b2df2d72f330143c67cf9390c31db34ff5b1
marcik357 <marcik357@gmail.com>
2023-07-16 22:01:21 +0300

    UPDATE: return contactForm from main

--- src/components/ContactForm/index.jsx
+++ src/components/ContactForm/index.jsx
@@ -1,11 +1,31 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styleText from '../../pages/Order/order.module.scss';
 import FormikForm from "../FormikForm";
 import { validationSchemaOrder } from "../../validation";
-
+import { useState, useEffect } from "react";
+import { setErrorAction } from "../../redux/actions/errorActions";
 export function ContactForm({ contactForm, setActive, active, setContactValue }) {
+  const dispatch = useDispatch();
   const customer = useSelector(state => state.cart.cart.customerId);
+  // const loading = useSelector(state => state.loading);
   const token = useSelector(state => state.token.token);
+  const [initialValues, setInitialValues] = useState({
+    name: '',
+    email: '',
+    telephone: ''
+  })
+
+  useEffect(() => {
+    try {
+      (token && customer) &&
+        setInitialValues(
+          initialValues.name = customer?.firstName,
+          initialValues.email = customer?.email,
+          initialValues.telephone = customer?.telephone)
+    } catch (error) {
+      return
+    }
+  }, [dispatch, customer, token, initialValues])
 
   const handleSubmit = (value) => {
     setActive(!active);
@@ -14,18 +34,13 @@ export function ContactForm({ contactForm, setActive, active, setContactValue })
 
   return (
     <>
-      {(token && customer) &&
-        <FormikForm
-          initialValues={{
-            name: customer?.firstName,
-            email: customer?.email,
-            telephone: customer?.telephone
-          }}
-          fields={contactForm}
-          validationSchema={validationSchemaOrder}
-          submitBtn="Continue"
-          callback={handleSubmit}
-        />}
+      <FormikForm
+        initialValues={initialValues}
+        fields={contactForm}
+        validationSchema={validationSchemaOrder}
+        submitBtn="Continue"
+        callback={handleSubmit}
+      />
       <h2 className={styleText.user_info__title}>Payment</h2>
     </>
   )
