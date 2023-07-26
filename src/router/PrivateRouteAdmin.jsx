ed9b703c3346eb79cb46e548114798c77fa398a3
Liliia Chekh <liliacheh@gmail.com>
2023-07-26 16:18:46 +0200

    Change: add 404 page

--- src/router/PrivateRouteAdmin.jsx
+++ src/router/PrivateRouteAdmin.jsx
@@ -1,34 +1,41 @@
 import { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { Link } from 'react-router-dom';
 import { getDataAction } from '../redux/actions/getDataActions';
 import { baseUrl } from '../utils/vars';
-import { AdminProducts } from '../pages';
-import { Arrow } from '../components/Icons';
+import { AdminProducts, NotFound } from '../pages';
 import { AdminLogin } from "../components/AdminLogin"
-import { NotAuthorizedAdmin } from '../components/NotAuthorizedAdmin';
+import Loader from '../components/Loader';
 
 export default function PrivateRouteAdmin() {
   const dispatch = useDispatch();
   const token = useSelector((state) => state.token.token) || localStorage.getItem('token');
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
-    token && dispatch(getDataAction(`${baseUrl}customers/customer`, setUser, {
-      method: "GET",
-      headers: {
-        Authorization: `Bearer ${token}`,
-        'Content-Type': 'application/json'
-      },
-    }, 'account-data'));
+    if (token) {
+      dispatch(getDataAction(`${baseUrl}customers/customer`, setUser, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          'Content-Type': 'application/json'
+        },
+      }, 'account-data'))
+      .finally(() => setIsLoading(false));
+    } else {
+      setIsLoading(false);
+    }
   }, [dispatch, token]);
 
-if (user?.isAdmin) {
+  if (isLoading) {
+    return <Loader/>;
+  }
+
+  if (user?.isAdmin) {
     return <AdminProducts />;
   } else if (token) {
-    return <NotAuthorizedAdmin/>
+    return <NotFound />;
   } else {
-    return <AdminLogin/>
+    return <AdminLogin />;
   }
-//   }
 }
