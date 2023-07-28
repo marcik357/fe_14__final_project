0ef1ec9f8ce553b380e3b7f8866b9d31443d0e1a
Anastasiia_Bl <blinova-nastia@ukr.net>
2023-07-28 11:39:37 +0300

    UPDATE: add fetch requests to discover page for loader correct work, made state for isOpen and saving it in sessionstorage, add condition for applyBtn disabled if both input empty

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,11 +1,57 @@
 import styles from './Discover.module.scss'
-import { useSelector } from 'react-redux';
+import { useState, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../../components/Banner';
 import Filter from '../../components/Filter';
 import Loader from '../../components/Loader';
+import { fetchData } from '../../utils';
+import { setErrorAction } from '../../redux/actions/errorActions';
+import { baseUrl } from '../../utils/vars';
+import { useNavigate } from 'react-router-dom';
+import { setLoadingAction } from '../../redux/actions/loadingActions';
+
 
 export function Discover() {
   const loading = useSelector((state) => state.loading.loading);
+  const queryString = useSelector((state) => state.filter.queryString);
+  const [products, setProducts] = useState([]);
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const renderProducts = useCallback(async () => {
+    try {
+      dispatch(setLoadingAction(true));
+      // Запит до API
+      const data = await fetchData(`${baseUrl}products/filter`)
+      await setProducts(data);
+      dispatch(setLoadingAction(false))
+    } catch (error) {
+      dispatch(setLoadingAction(false))
+      dispatch(setErrorAction(error.message));
+    }
+  }, [dispatch]);
+
+  useEffect(() => {
+    renderProducts();
+  }, [renderProducts]);
+
+  const applyFilters = useCallback(async () => {
+    try {
+      // Оновлення URL з актуальними параметрами фільтрації
+      navigate(`/discover?${queryString}`);
+      // Запит до API з використанням queryString для фільтрації товарів
+      const data = await fetchData(`${baseUrl}products/filter?${queryString}`)
+      await setProducts(data);
+    } catch (error) {
+      dispatch(setErrorAction(error.message));
+    }
+  }, [dispatch, queryString, navigate]);
+
+  useEffect(() => {
+    applyFilters(); // Викликати applyFilters при кожній зміні queryString
+  }, [applyFilters]);
+  console.log(products);
+  console.log(loading);
 
   if (loading) return <Loader />
 
@@ -17,7 +63,7 @@ export function Discover() {
         img='/images/banners/discover-banner.webp' />
       <div className={styles.products}>
         <div className={styles.products__container}>
-          <Filter />
+          <Filter products={products} />
         </div>
       </div>
     </div>
