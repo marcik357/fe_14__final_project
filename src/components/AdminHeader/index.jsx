da0b0ee9298c1a08d912aa3c42d60ef383dd6547
Liliia Chekh <liliacheh@gmail.com>
2023-07-28 16:27:50 +0200

    add logout button

--- src/components/AdminHeader/index.jsx
+++ src/components/AdminHeader/index.jsx
@@ -1,8 +1,17 @@
-import { Link } from "react-router-dom"
+import { Link, Navigate  } from "react-router-dom"
 import style from "./adminHeader.module.scss"
 import { Logo } from "../Icons"
+import { useDispatch } from "react-redux";
+import { setTokenAction } from "../../redux/actions/tokenActions";
 
 export default function AdminHeader () {
+  const dispatch = useDispatch();
+    
+    function logOut() {
+        localStorage.removeItem('token');
+        dispatch(setTokenAction(null));
+        return <Navigate to="/admin" />;
+      }
     return (
         <header className={style.header}>
             <div className={style.wrapper}>
@@ -15,6 +24,12 @@ export default function AdminHeader () {
                 </div>
               </Link>
               </div>
+              <button
+              className={style.logOut}
+              onClick={logOut}
+              type='button'>
+              Log out
+            </button>
         </div>
         </div>
         </header>
