8019f049562adaa6438af919e9880aa4554f9916
AnastasiiaBl <125908084+AnastasiiaBl@users.noreply.github.com>
2023-07-30 11:49:39 +0300

    New filter branch (#66)
    * UPDATE: add fetch requests to discover page for loader correct work, made state for isOpen and saving it in sessionstorage, add condition for applyBtn disabled if both input empty
    
    * UPDATE: change code for applyPriceFilter write another conditions for applyBtn
    
    * UPDATE: add media 481px for sidebarWrapper because of Header, change top and height sidebarWrapper for mobile, add transition for sidebarWrapper for 1024px
    
    * UPDATE: delete not used code
    
    * CHANGE: change styles for sidebarBckg and sidebarWrapper for mobile - now appear full size, delete not used code
    
    * UPDATE: add getFiltersByType and filters state in discover page, add fetchDataAndSetLoading function with promiseAll in discover page for on-off Loader

--- src/pages/Discover/index.jsx
+++ src/pages/Discover/index.jsx
@@ -1,11 +1,75 @@
 import styles from './Discover.module.scss'
-import { useSelector } from 'react-redux';
+import { useState, useCallback, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import Banner from '../../components/Banner';
 import Filter from '../../components/Filter';
 import Loader from '../../components/Loader';
+import { fetchData } from '../../utils';
+import { setErrorAction } from '../../redux/actions/errorActions';
+import { baseUrl } from '../../utils/vars';
+import { useNavigate } from 'react-router-dom';
+import { setLoadingAction } from '../../redux/actions/loadingActions';
+
 
 export function Discover() {
   const loading = useSelector((state) => state.loading.loading);
+  const queryString = useSelector((state) => state.filter.queryString);
+  const [products, setProducts] = useState([]);
+  const [filters, setFilters] = useState({
+    authorFilters: [],
+    categoriesFilters: [],
+    themeFilters: []
+  });
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const renderProducts = useCallback(async () => {
+    try {
+      // Запит до API
+      const data = await fetchData(`${baseUrl}products/filter`)
+      await setProducts(data);
+    } catch (error) {
+      dispatch(setErrorAction(error.message));
+    }
+  }, [dispatch]);
+
+  const getFiltersByType = useCallback(async (type) => {
+    try {
+      const data = await fetchData(`${baseUrl}filters/${type}`);
+      setFilters(prevFilters => ({
+        ...prevFilters,
+        [`${type}Filters`]: data
+      }));
+    } catch (error) {
+      dispatch(setErrorAction(error.message));
+    }
+  }, [dispatch]);
+
+  const fetchDataAndSetLoading = useCallback(async () => {
+    dispatch(setLoadingAction(true));
+    await Promise.all([renderProducts(), getFiltersByType('author'), getFiltersByType('categories'), getFiltersByType('theme')])
+    .then(() => dispatch(setLoadingAction(false)))
+  }, [dispatch, renderProducts, getFiltersByType]);
+
+  useEffect(() => {
+    fetchDataAndSetLoading();
+  }, [fetchDataAndSetLoading]);
+
+  const applyFilters = useCallback(async () => {
+    try {
+      // Оновлення URL з актуальними параметрами фільтрації
+      navigate(`/discover?${queryString}`);
+      // Запит до API з використанням queryString для фільтрації товарів
+      const data = await fetchData(`${baseUrl}products/filter?${queryString}`)
+      await setProducts(data);
+    } catch (error) {
+      dispatch(setErrorAction(error.message));
+    }
+  }, [dispatch, queryString, navigate]);
+
+  useEffect(() => {
+    applyFilters(); // Викликати applyFilters при кожній зміні queryString
+  }, [applyFilters]);
 
   if (loading) return <Loader />
 
@@ -17,7 +81,7 @@ export function Discover() {
         img='/images/banners/discover-banner.webp' />
       <div className={styles.products}>
         <div className={styles.products__container}>
-          <Filter />
+          <Filter products={products} filters={filters}/>
         </div>
       </div>
     </div>
