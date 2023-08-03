87ea38f63715c6f9343ef3abfa199b1c0ba2a801
marcik357 <marcik357@gmail.com>
2023-08-03 08:30:04 +0300

    UPDATE: some refactoring, fix and change backend url

--- src/components/MintBtn/index.jsx
+++ src/components/MintBtn/index.jsx
@@ -1,49 +1,53 @@
-// import style from './index.module.scss';
 import styles from '../Mint/MintPage.module.scss';
 import { useSelector, useDispatch } from "react-redux";
 import { baseUrl } from '../../utils/vars';
 import { setErrorAction } from '../../redux/actions/errorActions';
 import { fetchData } from '../../utils';
+import { reqDelete, reqPost } from '../../utils/requestBody';
 // import { cleanCart, createCartFromLS } from '../../redux/actions/cartActions';
 
-export function MintBtn({ orders, isOverlayVisible, card, user }) {
+export function MintBtn({ orders, isOverlayVisible, mintCard, user }) {
   const { token } = useSelector(state => state.token);
   const { cart } = useSelector(state => state.cart);
   const { mintCardFirst, mintCardSecond } = useSelector(state => state.mint);
   const dispatch = useDispatch();
 
-  function createMint(orders, selectCard) {
-    let order = orders.find((item) => item.products.some(product => product.product.itemNo === selectCard.itemNo) ? item.products : "")
-    order.products = order.products.filter(item => item.product.itemNo !== selectCard.itemNo),
-      order.email = "tester.crypter@gmail.com",
-      order.products.length > 1 ? changeOrder(token, order, order._id) : deleteOrder(token, order._id)
+  async function createMint(orders, selectCard) {
+    let order = orders.find((item) => item.products.some(product => product.product.itemNo === selectCard.itemNo) && item.products)
+    order.products = order.products.filter(item => item.product.itemNo !== selectCard.itemNo);
+    order.email = "tester.crypter@gmail.com";
+    order.products.length >= 1
+      ? await changeOrder(order, order._id)
+      : await deleteOrder(order._id)
   }
 
-  async function changeOrder(token, changedOrder, orderNumber) {
+  async function changeOrder(changedOrder, orderNumber) {
     try {
-      await fetchData(`${baseUrl}orders/${orderNumber}`, {
-        method: "PUT",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(changedOrder)
-      })
+      await fetchData(`${baseUrl}orders/${orderNumber}`, reqPost(JSON.stringify(changedOrder)));
+      // await fetchData(`${baseUrl}orders/${orderNumber}`, {
+      //   method: "PUT",
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(changedOrder)
+      // })
     } catch (error) {
       dispatch(setErrorAction(error.message));
     }
   }
 
-  async function deleteOrder(token, orderNumber) {
+  async function deleteOrder(orderNumber) {
     try {
-      await fetchData(`${baseUrl}orders/${orderNumber}`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(createMintOrder())
-      })
+      await fetchData(`${baseUrl}orders/${orderNumber}`, reqDelete());
+      // await fetchData(`${baseUrl}orders/${orderNumber}`, {
+      //   method: "DELETE",
+      //   headers: {
+      //     Authorization: `Bearer ${token}`,
+      //     'Content-Type': 'application/json'
+      //   },
+      //   body: JSON.stringify(createMintOrder())
+      // })
     } catch (error) {
       dispatch(setErrorAction(error.message));
     }
@@ -62,85 +66,103 @@ export function MintBtn({ orders, isOverlayVisible, card, user }) {
       customerId: user._id,
     }
   }
-  
-  async function sendMintOrder(card) {
-    if (cart?.products?.length > 0) {
-      const cartArray = cart?.products?.map(({ cartQuantity, product }) => {
-        return { product: product?._id, cartQuantity: cartQuantity }
-      })
-      await fetchData(`${baseUrl}cart`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
 
-      });
-      await fetchData(`${baseUrl}cart`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ products: [{ product: card._id, cartQuantity: 1 }] })
-      });
-      await fetchData(`${baseUrl}orders`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(createMintOrder())
-      });
-      await fetchData(`${baseUrl}cart`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
-      await fetchData(`${baseUrl}cart`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ products: cartArray })
-      });
-    }
-    else {
-      await fetchData(`${baseUrl}cart`, {
-        method: "POST",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ products: [{ product: card._id, cartQuantity: 1 }] })
-      });
-      await fetchData(`${baseUrl}orders`, {
-        method: "POST",
-        headers: {
-          'Content-Type': 'application/json',
-          Authorization: `Bearer ${token}`
-        },
-        body: JSON.stringify(createMintOrder())
-      })
-      await fetchData(`${baseUrl}cart`, {
-        method: "DELETE",
-        headers: {
-          Authorization: `Bearer ${token}`,
-          'Content-Type': 'application/json',
-        },
-      });
+  async function sendMintOrder(mintCard) {
+    try {
+      if (cart?.products?.length > 0) {
+        const cartArray = cart?.products?.map(({ cartQuantity, product }) => ({ product: product?._id, cartQuantity: cartQuantity }))
+
+        await fetchData(`${baseUrl}cart`, reqDelete());
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "DELETE",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        // });
+
+        await fetchData(`${baseUrl}cart`, reqPost(JSON.stringify({ products: [{ product: mintCard._id, cartQuantity: 1 }] })));
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "POST",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({ products: [{ product: mintCard._id, cartQuantity: 1 }] })
+        // });
+
+        await fetchData(`${baseUrl}orders`, reqPost(JSON.stringify(createMintOrder())));
+        // await fetchData(`${baseUrl}orders`, {
+        //   method: "POST",
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //     Authorization: `Bearer ${token}`
+        //   },
+        //   body: JSON.stringify(createMintOrder())
+        // });
+
+        await fetchData(`${baseUrl}cart`, reqDelete());
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "DELETE",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        // });
+
+        await fetchData(`${baseUrl}cart`, reqPost(JSON.stringify({ products: cartArray })));
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "POST",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({ products: cartArray })
+        // });
+      }
+      else {
+        await fetchData(`${baseUrl}cart`, reqPost(JSON.stringify({ products: [{ product: mintCard._id, cartQuantity: 1 }] })));
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "POST",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        //   body: JSON.stringify({ products: [{ product: mintCard._id, cartQuantity: 1 }] })
+        // });
+
+        await fetchData(`${baseUrl}orders`, reqPost(JSON.stringify(createMintOrder())));
+        // await fetchData(`${baseUrl}orders`, {
+        //   method: "POST",
+        //   headers: {
+        //     'Content-Type': 'application/json',
+        //     Authorization: `Bearer ${token}`
+        //   },
+        //   body: JSON.stringify(createMintOrder())
+        // })
+
+        await fetchData(`${baseUrl}cart`, reqDelete());
+        // await fetchData(`${baseUrl}cart`, {
+        //   method: "DELETE",
+        //   headers: {
+        //     Authorization: `Bearer ${token}`,
+        //     'Content-Type': 'application/json',
+        //   },
+        // });
+      }
+    } catch (error) {
+      console.log(error);
+      dispatch(setErrorAction(error.message));
     }
   }
+
   return (
     <button
       className={isOverlayVisible && styles.mintPage__hiddenButton_text}
-      onClick={() => {
-        createMint(orders, mintCardFirst);
-        createMint(orders, mintCardSecond);
-        sendMintOrder(card);
+      onClick={async () => {
+        await createMint(orders, mintCardFirst);
+        await createMint(orders, mintCardSecond);
+        await sendMintOrder(mintCard);
       }}>
       Mint
     </button>
